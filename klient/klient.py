983912574a40b802b3e6495eaea8c83ef7d7d6ac
OKrass <OKrass@users.noreply.github.com>
2018-03-27 19:32:17 +0200

    message update

--- klient/klient.py
+++ klient/klient.py
@@ -4,12 +4,12 @@ import sys
 TCP_IP = '127.0.0.1'
 TCP_PORT = 3001
 BUFFER_SIZE = 1024
-message = 'sasaqt'
+message = 'all data accepted\t'
 
 try:
-    print >> sys.stderr, 'connection from', TCP_IP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
+    print >> sys.stderr, 'connection from', TCP_IP
         # Receive the data and print it
     while True:
         data = s.recv(BUFFER_SIZE)
@@ -17,6 +17,7 @@ try:
         if data:
             continue
         else:
+            s.send(message)
             print >> sys.stderr, 'no more data from', TCP_IP
             break
 
