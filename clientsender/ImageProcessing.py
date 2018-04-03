b618113dbb0405531d9782e6f0d9fdca02244c36
OKrass <OKrass@users.noreply.github.com>
2018-04-03 02:10:11 +0200

    ImageProccesing is now a function which gets mean values of bgr channels, mean of n frames. clientsender tries to start in 2 threads imageProcessing and information sending. (currently not working)

--- clientsender/ImageProcessing.py
+++ clientsender/ImageProcessing.py
@@ -1,92 +1,162 @@
 import numpy as np
 import cv2 as cv
-
+import multiprocessing
+import thread
 Blue = []
 Green = []
 Red = []
-count = 0
+q = multiprocessing.Queue()
 
-#Set Number of Frames between 50 - 350
-NumberOfFrames = 5
+# Set Number of Frames between 50 - 350
+NumberOfFrames = 20
 
-#Getting mean of an image and later adding it to an array
-def ArrayAppend(ColorFrame,ChannelName):
+# Getting mean of an image and later adding it to an array
+
+
+def ArrayAppend(ColorFrame, ChannelName):
     l = ColorFrame.mean()
     ChannelName.append(l)
     return ChannelName
-#Splitting image and using ArrayAppend to add mean value ro
-def ImageSplit(roi):
-    b, g, r = cv.split(roi)
-    BlueArray = ArrayAppend(b, Blue)
-    GreenArray = ArrayAppend(g, Green)
-    RedArray = ArrayAppend(r, Red)
-    return BlueArray, GreenArray, RedArray
+
+# Splitting image and using ArrayAppend to add mean value ro
+
+
+def ImageSplit(ROI):
+    b, g, r = cv.split(ROI)
+    BlueArraytemp = ArrayAppend(b, Blue)
+    GreenArraytemp = ArrayAppend(g, Green)
+    RedArraytemp = ArrayAppend(r, Red)
+    return BlueArraytemp, GreenArraytemp, RedArraytemp
+
 
 def GetArrayMean(ColorArray):
     JustinCase = ColorArray
     MeanOfArray = np.mean(JustinCase)
     return MeanOfArray
 
+
 def FFT(ChannelArray):
     return np.fft.fft(ChannelArray)
-    #FFT dor all 3 channels
+
+    # FFT dor all 3 channels
 def ChannelFFT(BlueArray, GreenArray, RedArray):
     Tempb = BlueArray
     Tempg = GreenArray
     Tempr = RedArray
-    BlueFFT = FFT(Tempb)
-    GreenFFT = FFT(Tempg)
-    RedFFT = FFT(Tempr)
-    return BlueFFT ,GreenFFT, RedFFT
-#Preparing data which are going to be sent
-def DataToSend(BlueArray, GreenArray, RedArray):
+    blueFFT = FFT(Tempb)
+    greenFFT = FFT(Tempg)
+    redFFT = FFT(Tempr)
+    return blueFFT, greenFFT, redFFT
+
+
+# Preparing data which are going to be sent
+def DataToSend(BlueArray, GreenArray, RedArray, i):
+    counter = i
+    check = counter % 10
     MeanOfBlue = GetArrayMean(BlueArray)
     MeanOfGreen = GetArrayMean(GreenArray)
     MeanOfRed = GetArrayMean(RedArray)
-    temp = [MeanOfBlue, MeanOfGreen, MeanOfRed]
+    # For i = 9 we are sending all information ( mean of 3 channels(r,g,b), mean of mean of (r,g,b), FFT of 3 channels )
+    if check == 9:
+        BlueFFT, GreenFFT, RedFFT = ChannelFFT(BlueArray, GreenArray, RedArray)
+        temp1 = [MeanOfBlue, MeanOfGreen, MeanOfRed, Meanof3channels(MeanOfBlue, MeanOfGreen, MeanOfRed)]
+        return temp1, 1, BlueFFT, GreenFFT, RedFFT
+    else:
+        BlueFFT, GreenFFT, RedFFT = ChannelFFT([0],[0],[0])
+        temp = [];
+        return temp, 0, BlueFFT, GreenFFT, RedFFT
+
+
+def Meanof3channels(MeanOfblue, MeanOfgreen, MeanOfred):
+    temp = [MeanOfblue, MeanOfgreen, MeanOfred]
     Meanof3channels = GetArrayMean(temp)
-    temp = [MeanOfBlue, MeanOfGreen, MeanOfRed, Meanof3channels]
-    return temp
+    return Meanof3channels
+
+
+def PutAllDataInQueue(Queue,Color, New, Mean, BFFT, GFFT, RFFT):
+
+    Queue.put_nowait(Color[0])
+    Queue.put_nowait(Color[1])
+    Queue.put_nowait(Color[2])
+    Queue.put_nowait(New)
+    if not Mean:
+        Queue.put_nowait(0)
+        Queue.put_nowait(0)
+        Queue.put_nowait(0)
+        Queue.put_nowait(0)
+    else:
+        Queue.put_nowait(Mean[0])
+        Queue.put_nowait(Mean[1])
+        Queue.put_nowait(Mean[2])
+        Queue.put_nowait(Mean[3])
+    Queue.put_nowait(BFFT)
+    Queue.put_nowait(GFFT)
+    Queue.put_nowait(RFFT)
+    print Queue.qsize()
+
+    Queue.close()
+    Queue.join_thread()
+
 
 def ArrayClearing():
     return 0, 0, 0
 
-#set number of frames
+def RUN(NumberOfFrames,Lock):
+    count = 0
+    cap = cv.VideoCapture(0)
+    while 1:
+        reg, video = cap.read()
+        font = cv.FONT_HERSHEY_SIMPLEX
+        cv.putText(video, 'Press q when ready to choose area', (30, 40), font, 1, (255, 255, 0), 2, cv.LINE_AA)
+        cv.imshow('Video', video)
 
-cap = cv.VideoCapture(0)
-# take first frame of the video
-ret, frame = cap.read()
-# setup initial location of window
-c, r, w, h = cv.selectROI('Image', frame)
-track_window = (c, r, w, h)
-# set up the ROI for getting info
-roi = frame[r:r+h, c:c+w]
-
-# Crop image
-while(1):
-    if count > NumberOfFrames:
-        BlueFFT, GreenFFT, RedFFT = ChannelFFT(BlueArray, GreenArray, RedArray)
-        Message = DataToSend(BlueArray, GreenArray, RedArray)
-        #Clearing Arrays and counter
-        BlueArray, GreenArray, RedArray = ArrayClearing()
-        BlueFFT, GreenFFT, RedFFT = ArrayClearing()
-        count = 0
-        continue
-    else:
-        ret ,frame = cap.read()
-        if ret == True:
-            #Getting video from selected location
-            x, y, w, h = track_window
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            roi = frame[y:y + h, x:x + w]
-            cv.imshow('dst', roi)
-            # Spliting image and getting mean of a frame into arrays
-            BlueArray, GreenArray, RedArray = ImageSplit(roi)
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
+        # Press q if ready to continue
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            # closing useless window
+            cv.destroyAllWindows()
             break
-        count += 1
-cv.destroyAllWindows()
-cap.release()
+    # take first frame of the video
+    ret, frame = cap.read()
+    # setup initial location of window
+    c, r, w, h = cv.selectROI('Image', frame, 0)
+    track_window = (c, r, w, h)
+    # set up the ROI for getting info
+    roi = frame[r:r+h, c:c+w]
+    # closing useless window
+    cv.destroyAllWindows()
+    # Crop image
+    while 1:
+        if count > NumberOfFrames:
 
+            # Clearing Arrays and counter
+            del BlueArray[:], GreenArray[:], RedArray[:], Blue[:], Red[:], Green[:]
+            BlueFFT, GreenFFT, RedFFT = ArrayClearing()
+            count = 0
+            continue
+        else:
+            ret, frame = cap.read()
+            if ret is True:
+                # Getting video from selected location
+                x, y, w, h = track_window
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                roi = frame[y:y + h, x:x + w]
+
+                cv.imshow('dst', roi)
+                # Splitting image and getting mean of a frame into arrays
+                BlueArray, GreenArray, RedArray = ImageSplit(roi)
+                BGR = [BlueArray[count], GreenArray[count], RedArray[count]]
+                # getting all needed information
+                mean, isnew, msgBlueFFT, msgGreenFFT, msgRedFFT = DataToSend(BlueArray, GreenArray, RedArray, count)
+                # Putting all data in queue
+                Lock.acquire
+                PutAllDataInQueue(Lock, q, BGR, isnew, mean, msgBlueFFT, msgGreenFFT, msgRedFFT)
+                Lock.release
+                BGR = []
+                count += 1
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+    cv.destroyAllWindows()
+    cap.release()
--- clientsender/clientsender.py
+++ clientsender/clientsender.py
@@ -2,67 +2,74 @@ import socket
 import sys
 import time
 import thread
+import ImageProcessing
+import multiprocessing
+
 # Put your IP and port here
+q = multiprocessing.Queue()
 TCP_IP = '127.0.0.1'
 TCP_PORT = 3001
 buffersize = 256
+Data = []
+NumberOfFrames = 50
+Lock = thread.allocate_lock()
 # Set amount of time between sending data in s
 timeBetweenTransmission = 2
-#Data which are going to be sent
-data = 'SOME RATA'
-#Function to get data later
-def getData():
-    data = 0
-    return data
+
+def getData(queue,data):
+    while not queue.empty():
+        data.append(queue.get())
+        queue.task_done()
     # Send data via TCP
-def sendData(data,t):
+
+def sendData(t):
     gettime = t
-    informationToSent = data
+
     print('Started sending messages')
     while True:
-
-        s.send(informationToSent)
-
-        time.sleep(gettime)
-        try:
-            answer = s.recv(buffersize)
-            #Checking if sent information is same as recieved
-            if (answer != informationToSent):
-                print('messages do not match')
-                s.close()
-                sys.exit(0)
-
-
-
-        except Exception:
-            print('something went wrong')
-            s.close()
-        #Getting new information
-        #informationToSent = getData()
-
-
-        if informationToSent:
+        getData(q, Data)
+        if not Data:
             continue
         else:
-            print 'no more data to %s\nclosing '%(TCP_IP)
-            s.close()
-            sys.exit(0)
-        break
+            s.send(Data)
+            time.sleep(gettime)
+            try:
+                answer = s.recv(buffersize)
+                # Checking if sent information is same as received
+                if (answer != Data):
+                    print('messages do not match')
+                    s.close()
+                    sys.exit('error')
+
+            except Exception:
+                print('something went wrong')
+                s.close()
+            #Getting new information
+            #informationToSent = getData()
+
+
+            if Data:
+                continue
+            else:
+                print 'no more data to %s\nclosing '%(TCP_IP)
+                s.close()
+                sys.exit(0)
+            break
 
 try:
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #COnnecting to server using IP and Port
     s.connect((TCP_IP, TCP_PORT))
     print 'connecting to %s' % (TCP_IP)
-    thread.start_new_thread(sendData, (data, timeBetweenTransmission,))
+
+    thread.start_new_thread(sendData, (timeBetweenTransmission,))
+    thread.start_new_thread(ImageProcessing.RUN(NumberOfFrames, Lock))
+
+
+
 except:
     print "Error: unable to start thread"
     sys.exit(1)
 while 1:
    pass
-
-
-
-
-
-
